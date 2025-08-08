32dc564f781c21eecfe94da9b1924bb945ac4c55
Tvisha Sharma <tvisha.edu@gmail.com>
2025-08-08 18:32:05 +0530

    feat: add EventCalender component and integrate it into AdminPage layout

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from '@/components/AttendanceChart'
 import CountChart from '@/components/CountChart'
+import EventCalender from '@/components/EventCalender'
 import FinanceChart from '@/components/FinanceChart'
 import UserCard from '@/components/UserCard'
 import React from 'react'
@@ -35,7 +36,9 @@ const AdminPage = () => {
         <div className=''></div>
       </div>
       {/* RIGHT */}
-      <div className='w-full lg:w-1/3'>r</div>
+      <div className='w-full lg:w-1/3 flex flex-col gap-8'>
+        <EventCalender/>                                                                                
+      </div>
     </div>
   )
 }
