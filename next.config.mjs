8519903de26f85880d4bb2997c1c07f5c6b5578b
Tvisha Sharma <tvisha.edu@gmail.com>
2025-08-12 17:57:18 +0530

    feat: enhance TeachersListPage with table component and search functionality

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{hostname: "images.pexels.com"}], // whitelist Pexels
+  },
+};
 
 export default nextConfig;
