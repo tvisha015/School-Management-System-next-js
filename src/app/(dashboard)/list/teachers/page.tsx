690dc882f49dcea78ab4103ba38ed919a1c0d86d
Tvisha Sharma <tvisha.edu@gmail.com>
2025-08-12 11:20:38 +0530

    feat: create TeachersListPage with pagination and search functionality

---
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -0,0 +1,35 @@
+import Pagination from '@/components/Pagination'
+import TableSearch from '@/components/TableSearh'
+import Image from 'next/image'
+import React from 'react'
+
+const TeachersListPage = () => {
+  return (
+    <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
+        {/* TOP */}
+        <div className="flex items-center justify-between">
+            <h1 className='hidden md:block text-lg font-semibold'>All teachers</h1>
+            <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+                <TableSearch/>
+                <div className="flex items-center gap-4 self-end">
+                    <button className='w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow'>
+                        <Image src="/filter.png" alt='' width={14} height={14}/>
+                    </button>
+                    <button className='w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow'>
+                        <Image src="/sort.png" alt='' width={14} height={14}/>
+                    </button>
+                    <button className='w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow'>
+                        <Image src="/plus.png" alt='' width={14} height={14}/>
+                    </button>
+                </div>
+            </div>
+        </div>
+        {/* LIST */}
+        <div className=""></div>
+        {/* PAGINATION */}
+        <Pagination/>
+    </div>
+  )
+}
+
+export default TeachersListPage
