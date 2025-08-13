266d4e2501f2ee09c8b57f356ca93c5fc5379ae9
Tvisha Sharma <tvisha.edu@gmail.com>
2025-08-13 16:06:17 +0530

    feat: Implement new pages for announcements, assignments, classes, events, exams, lessons, parents, results, students, and subjects with responsive tables and pagination

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -1,20 +1,20 @@
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { role, teachersData } from "@/lib/data";
+import { role, studentsData } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-type Teacher = {
+type Student = {
   id: number;
-  teacherId: string;
+  studentId: string;
   name: string;
   email?: string;
   photo: string;
-  phone: string;
-  subjects: string[];
-  classes: string[];
+  phone?: string;
+  grade: number;
+  class: string;
   address: string;
 };
 
@@ -24,29 +24,24 @@ const columns = [
     accessor: "info",
   },
   {
-    header: "Teacher ID",
-    accessor: "teacherId",
-    className: "hidden md:table-cell text-xs md:text-sm lg:text-base",
+    header: "Student ID",
+    accessor: "studentId",
+    className: "hidden md:table-cell",
   },
   {
-    header: "Subjects",
-    accessor: "subjects",
-    className: "hidden md:table-cell text-xs md:text-sm lg:text-base",
-  },
-  {
-    header: "Classes",
-    accessor: "classes",
-    className: "hidden md:table-cell text-xs md:text-sm lg:text-base",
+    header: "Grade",
+    accessor: "grade",
+    className: "hidden md:table-cell",
   },
   {
     header: "Phone",
     accessor: "phone",
-    className: "hidden lg:table-cell text-xs md:text-sm lg:text-base",
+    className: "hidden lg:table-cell",
   },
   {
     header: "Address",
     accessor: "address",
-    className: "hidden lg:table-cell text-xs md:text-sm lg:text-base",
+    className: "hidden lg:table-cell",
   },
   {
     header: "Actions",
@@ -55,8 +50,8 @@ const columns = [
 ];
 
 
-const TeachersListPage = () => {
-  const renderRow = (item: Teacher) => (
+const StudentsListPage = () => {
+  const renderRow = (item: Student) => (
     <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
       <td className="flex items-center gap-4 p-4">
         <Image
@@ -68,12 +63,11 @@ const TeachersListPage = () => {
         />
         <div className="flex flex-col">
           <h3 className="font-semibold">{item.name}</h3>
-          <p className="text-xs text-gray-500">{item?.email}</p>
+          <p className="text-xs text-gray-500">{item.class}</p>
         </div>
       </td>
-      <td className="hidden md:table-cell">{item.teacherId}</td>
-      <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
-      <td className="hidden md:table-cell">{item.classes.join(",")}</td>
+      <td className="hidden md:table-cell">{item.studentId}</td>
+      <td className="hidden md:table-cell">{item.grade}</td>
       <td className="hidden md:table-cell">{item.phone}</td>
       <td className="hidden md:table-cell">{item.address}</td>
       <td>
@@ -95,7 +89,7 @@ const TeachersListPage = () => {
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Students</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -112,11 +106,11 @@ const TeachersListPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={teachersData}/>
+      <Table columns={columns} renderRow={renderRow} data={studentsData}/>
       {/* PAGINATION */}
       <Pagination />
     </div>
   );
 };
 
-export default TeachersListPage;
+export default StudentsListPage;
