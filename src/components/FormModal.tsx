f0d2703661cc28b94349cb122a915b13d4f22d77
Tvisha Sharma <tvisha.edu@gmail.com>
2025-08-19 13:28:35 +0530

    feat: Add TeacherForm component with validation and InputField integration

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 
 // USE LAZY LOADING
 
-// import TeacherForm from "./forms/TeacherForm";
+import TeacherForm from "./forms/TeacherForm";
 // import StudentForm from "./forms/StudentForm";
 
 // const TeacherForm = dynamic(() => import("./forms/TeacherForm"), {
@@ -16,12 +16,12 @@ import { useState } from "react";
 //   loading: () => <h1>Loading...</h1>,
 // });
 
-// const forms: {
-//   [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
-// } = {
-//   teacher: (type, data) => <TeacherForm type={type} data={data} />,
-//   student: (type, data) => <StudentForm type={type} data={data} />
-// };
+const forms: {
+  [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
+} = {
+  teacher: (type, data) => <TeacherForm type={type} data={data} />,
+  // student: (type, data) => <StudentForm type={type} data={data} />
+};
 
 const FormModal = ({
   table,
@@ -56,22 +56,27 @@ const FormModal = ({
 
   const [open, setOpen] = useState(false);
 
-//   const Form = () => {
-//     return type === "delete" && id ? (
-//       <form action="" className="p-4 flex flex-col gap-4">
-//         <span className="text-center font-medium">
-//           All data will be lost. Are you sure you want to delete this {table}?
-//         </span>
-//         <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">
-//           Delete
-//         </button>
-//       </form>
-//     ) : type === "create" || type === "update" ? (
-//       forms[table](type, data)
-//     ) : (
-//       "Form not found!"
-//     );
-//   };
+  const Form = () => {
+    return type === "delete" && id ? (
+      <form action="" className="p-4 flex flex-col gap-4">
+        <span className="text-center font-medium">
+          All data will be lost. Are you sure you want to delete this {table}?
+        </span>
+        <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">
+          Delete
+        </button>
+      </form>
+    )
+    : (
+      <TeacherForm type="update" data={data}/> 
+    ); 
+    // : type === "create" || type === "update" ? (
+    //   Form[table](type, data)
+    // ) 
+    // : (
+    //   "Form not found!"
+    // );
+  };
 
   return (
     <>
@@ -84,7 +89,7 @@ const FormModal = ({
       {open && (
         <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
           <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
-            {/* <Form /> */}
+            <Form />
             <div
               className="absolute top-4 right-4 cursor-pointer"
               onClick={() => setOpen(false)}
